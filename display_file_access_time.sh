4bbba476f74e31bca0efbef1030259ab162363e5
SugumaranEvil <suguanbu321@gmail.com>
2021-03-24 12:54:28 +0530

    Display file name & timestamp of a file

---
+++ display_file_access_time.sh
@@ -0,0 +1,24 @@
+#**********************************************************************#
+# Author      : Sugumaran A                                            #
+# File Name   : display_file_access_time.sh                            #
+# Description : printing last access time & file name by user input..! #
+# Date        : 24-03-2021                                             #
+#**********************************************************************#
+
+
+#!/bin/bash
+
+echo Enter the path name
+read path_name
+
+echo Enter the day you want know last access of the files or directories 
+read day
+
+#find  - search for files in a directory hierarchy
+#atime - File  was  last  accessed  n*24 hours ago
+#print - action is performed on all files
+#printf %c - locale's  date  and  time 
+
+find $path_name -atime -$day -print -printf %c
+
+exit 0
