0e8ab6d274019850e81b4782ca9cbb844916a76c
SugumaranEvil <suguanbu321@gmail.com>
2021-03-24 13:19:53 +0530

    BASH Script........>>>>!

--- display_file_access_time.sh
+++ display_file_access_time.sh
@@ -11,7 +11,7 @@
 echo Enter the path name
 read path_name
 
-echo Enter the day you want know last access of the files or directories within that days 
+echo "Enter a number of day(s) from present day to get accessed details of file or directory"
 read day
 
 #find  - search for files in a directory hierarchy
